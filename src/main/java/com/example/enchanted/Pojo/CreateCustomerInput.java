24283d879021c55dcd48f57ca65b1439ea99bf93
KaltrinaI <k.ismaili2001@gmail.com>
2024-05-19 16:12:17 +0200

    Adding Validation

--- src/main/java/com/example/enchanted/Pojo/CreateCustomerInput.java
+++ src/main/java/com/example/enchanted/Pojo/CreateCustomerInput.java
@@ -1,11 +1,24 @@
 package com.example.enchanted.Pojo;
 
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 public class CreateCustomerInput {
+    @NotEmpty(message = "Name must not be empty")
     private String name;
+    @Email(message = "Email should be valid")
+    @NotEmpty(message = "Email must not be empty")
     private String email;
+
+    @NotEmpty(message = "Phone number must not be empty")
     private String address;
+    @NotEmpty(message = "Address must not be empty")
     private String phoneNumber;
+
+    public CreateCustomerInput(){
+
+    }
 
     public CreateCustomerInput(String name, String email, String address, String phoneNumber) {
         this.name = name;
--- src/main/java/com/example/enchanted/Service/InsufficientAmountException.java
+++
@@ -1,4 +0,0 @@
-package com.example.enchanted.Service;
-
-public class InsufficientAmountException extends Exception {
-}
